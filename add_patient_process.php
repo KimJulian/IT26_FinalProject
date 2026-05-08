a624d5ed4938f91c7c67995822a54ac08645b5af
itsmeurky <michaelladrenoio@gmail.com>
2026-05-08 12:49:39 +0800

    update backend processing to store student academic data and medication history

--- add_patient_process.php
+++ add_patient_process.php
@@ -5,14 +5,17 @@ $conn = new mysqli("localhost", "root", "", "healthfile_db");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['patient_name'];
+    $course = $_POST['course'];
+    $sy = $_POST['school_year'];
+    $date = $_POST['date_recorded'];
     $diagnosis = $_POST['diagnosis'];
+    $meds = $_POST['meds_given'];
     $doctor_id = $_POST['doctor_id'];
     $status = "Active";
     $date = date('Y-m-d');
 
-    $sql = "INSERT INTO patients (patient_name, admission_date, diagnosis, doctor_id, status) 
-            VALUES ('$name', '$date', '$diagnosis', $doctor_id, '$status')";
-
+    $sql = "INSERT INTO patients (patient_name, course, school_year, date_recorded, diagnosis, meds_given, doctor_id, status) 
+        VALUES ('$name', '$course', '$sy', '$date', '$diagnosis', '$meds', '$doctor_id', 'Active')";
     if ($conn->query($sql) === TRUE) {
         header("Location: dashboard.php?msg=PatientAdded");
     } else {
